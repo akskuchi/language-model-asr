379f83465d30e51a5da9cbcb45f7bb9c069489ac
Sam Spilsbury <smspillaz@gmail.com>
2018-11-22 23:06:13 +0200

    main: Add a small mitigation to prevent running out of memory
    The metrics need CUDA memory and if we don't limit the amount of
    batches we measure, torch may end up eating more CUDA memory than it
    can afford, leading to exceptions.

--- Baseline_edu/src2/main.py
+++ Baseline_edu/src2/main.py
@@ -152,7 +152,8 @@ def evaluate(data_source):
     ntokens = len(corpus.dictionary)
     hidden = model.init_hidden(eval_batch_size)
     with torch.no_grad():
-        for i in range(0, data_source.size(0) - 1, args.bptt):
+        # Need this otherwise pytorch runs out of memory
+        for i in range(0, min(data_source.size(0) - 1, 1000), args.bptt):
             data, targets = get_batch(data_source, i)
             output, hidden = model(data, hidden)
             output_flat = output.view(-1, ntokens)
